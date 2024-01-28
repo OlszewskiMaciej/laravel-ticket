4f45ebee2ffc0ea4fe9112dc4d07fbbeeaadd71e
OlszewskiMaciej <maciej.olszewski4@gmail.com>
2024-01-28 18:30:39 +0100

    ticket css changes

--- resources/views/ticket/index.blade.php
+++ resources/views/ticket/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
         <h1 class="text-white text-lg font-bold">Tickets list</h1>
-        <div class="w-full sm:max-w-xl mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+        <div class="w-full sm:max-w-2xl mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
 
             <div class="flex items-center justify-end mt-4 py-4">
                 <x-primary-button class="ml-3">
@@ -12,11 +12,12 @@
             </div>
 
             @forelse ($tickets as $ticket)
-
-            <div class="text-white flex justify-between py-4">
-                <a href="{{route('ticket.show', $ticket->id)}}">{{$ticket->title}}</a>
-                <p>{{$ticket->created_at->diffForHumans()}}</p>
-            </div>
+            <a href="{{route('ticket.show', $ticket->id)}}">
+                <div class="text-white flex justify-between py-4 hover:bg-gray-200 dark:hover:bg-gray-700 transition-all duration-300 rounded">
+                    <p class="px-2">{{$ticket->title}}</p>
+                    <p class="px-2">{{$ticket->created_at->diffForHumans()}}</p>
+                </div>
+            </a>
 
             @empty
 
