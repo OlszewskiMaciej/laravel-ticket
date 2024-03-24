581e4bc83cff6ff1c95b7075f64832a9039ca81c
OlszewskiMaciej <maciej.olszewski4@gmail.com>
2024-03-24 19:27:46 +0100

    test

--- config/database.php
+++ config/database.php
@@ -63,17 +63,6 @@ return [
             ]) : [],
         ],
 
-        'testing' => [
-            'driver' => 'mysql',
-            'host' => env('DB_TEST_HOST', '127.0.0.1'),
-            'port' => env('DB_TEST_PORT', '3306'),
-            'database' => env('DB_TEST_DATABASE', 'forge'),
-            'username' => env('DB_TEST_USERNAME', 'forge'),
-            'password' => env('DB_TEST_PASSWORD', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-        ],
-
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
